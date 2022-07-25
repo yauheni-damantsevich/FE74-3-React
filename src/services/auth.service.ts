25fe79cd3d6adbdacf434de91f10865ee233a252
yauheni-damantsevich <yauheni.damantsevich@outlook.com>
2022-07-25 19:20:58 +0300

    fix auth duplicate functions

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,25 +1,6 @@
 import axios from "axios";
 const API_URL = "https://studapi.teachmeskills.by/auth/";
 
-export function login(email: string, password: string) {
-  return axios
-    .post(API_URL + "jwt/create/", {
-      email,
-      password,
-    })
-    .then((response) => {
-      console.log(response);
-
-      // if (response.data.accessToken) {
-      //   localStorage.setItem("user", JSON.stringify(response.data));
-      //   console.log(response);
-      // }
-      // return response.data;
-    });
-}
-export function logout() {
-  localStorage.removeItem("user");
-}
 export function register(username: string, email: string, password: string) {
   return axios.post(API_URL + "users/", {
     username,
